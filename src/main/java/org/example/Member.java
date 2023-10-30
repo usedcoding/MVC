bf71529024f4c2a24796e3b786cf3cd888f384da
usedcoding <ja14759@gmail.com>
2023-10-30 12:05:48 +0900

    MVC

---
+++ src/main/java/org/example/Member.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Member {
+    private String userId;
+    private String password;
+    private String confirmPassword;
+
+}
---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class WiseSaying {
+    private long id;
+    private String title;
+    private String content;
+}
+
+

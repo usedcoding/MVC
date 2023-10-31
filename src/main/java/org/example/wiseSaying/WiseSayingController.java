fb2e9f3dcf40750838cee42dfcc8d3638987d082
usedcoding <ja14759@gmail.com>
2023-10-31 23:14:11 +0900

    service error

--- src/main/java/org/example/wiseSaying/WiseSayingController.java
+++ src/main/java/org/example/wiseSaying/WiseSayingController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingController {
-    List<WiseSaying> wiseSayingList = new ArrayList<>();
-    long id = 0;
+
+
     Scanner sc;
+
+    WiseSayingService wiseSayingService = new WiseSayingService();
 
     public WiseSayingController(Scanner sc) {
         this.sc = sc;
@@ -15,21 +17,21 @@ public class WiseSayingController {
 
 
     public void create() {
-        id++;
+
         System.out.print("제목 ) ");
         String title = sc.nextLine();
 
         System.out.print("내용 ) ");
         String content = sc.nextLine();
 
-        WiseSaying wiseSaying = new WiseSaying(id, title, content);
-        wiseSayingList.add(wiseSaying);
+        long id = wiseSayingService.create(title, content);
 
         System.out.println(id + "번 게시글이 등록 되었습니다.");
 
     }
 
     public void list() {
+       List<WiseSaying> wiseSayingList = wiseSayingService.getWiseSayingListAll();
         System.out.println("번호 / 제목 / 내용");
         for (int i = 0; i < wiseSayingList.size(); i++) {
             WiseSaying wiseSaying = wiseSayingList.get(i);
@@ -42,12 +44,9 @@ public class WiseSayingController {
         System.out.println("삭제할 번호를 입력해 주세요 ) ");
         long removeId = sc.nextLong();
         sc.nextLine();
+        WiseSaying wiseSaying = this.wiseSayingService.wiseSayingFindById(removeId);
 
-        WiseSaying wiseSaying = wiseSayingFindById(id);
-
-        if (wiseSaying.getId() == removeId) {
-            wiseSayingList.remove(wiseSaying);
-        }
+        this.wiseSayingService.remove(wiseSaying);
     }
 
 
@@ -56,29 +55,22 @@ public class WiseSayingController {
         long modifyId = sc.nextLong();
         sc.nextLine();
 
-        WiseSaying wiseSaying = wiseSayingFindById(id);
+        WiseSaying wiseSaying =this.wiseSayingService.wiseSayingFindById(modifyId);
 
         if (wiseSaying.getId() == modifyId) {
             System.out.println("기존 제목 : " + wiseSaying.getTitle());
             System.out.println("수정 ) ");
             String title = sc.nextLine();
-            wiseSaying.setTitle(title);
+
 
             System.out.println("기존 내용 : " + wiseSaying.getContent());
             System.out.println("수정 ) ");
             String content = sc.nextLine();
-            wiseSaying.setContent(content);
+
+            this.wiseSayingService.modify(wiseSaying, title, content);
 
         }
 
     }
-    private WiseSaying wiseSayingFindById(long id) {
-        for(int i = 0; i < wiseSayingList.size(); i++) {
-            WiseSaying wiseSaying = wiseSayingList.get(i);
-            if(wiseSaying.getId() == id) {
-                return wiseSaying;
-            }
-        }
-        return null;
-    }
+
 }
--- src/main/java/org/example/wiseSaying/WiseSayingService.java
+++ src/main/java/org/example/wiseSaying/WiseSayingService.java
@@ -1,4 +1,45 @@
 package org.example.wiseSaying;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WiseSayingService {
+    long lastId = 0;
+    List<WiseSaying> wiseSayingList = new ArrayList<>();
+
+
+    public long create(String title, String content) {
+
+        lastId++;
+        WiseSaying wiseSaying = new WiseSaying(lastId, title, content);
+        wiseSayingList.add(wiseSaying);
+
+        return lastId;
+    }
+
+    public List<WiseSaying> getWiseSayingListAll() {
+        return wiseSayingList;
+    }
+
+
+
+    public void remove(WiseSaying wiseSaying) {
+       wiseSayingList.remove(wiseSaying);
+
+    }
+
+    public void modify(WiseSaying wiseSaying, String title, String content) {
+        wiseSaying.setTitle(title);
+        wiseSaying.setContent(content);
+    }
+
+    public WiseSaying wiseSayingFindById(long id) {
+        for(int i = 0; i < wiseSayingList.size(); i++) {
+            WiseSaying wiseSaying = wiseSayingList.get(i);
+            if(wiseSaying.getId() == id) {
+                return wiseSaying;
+            }
+        }
+        return null;
+    }
 }

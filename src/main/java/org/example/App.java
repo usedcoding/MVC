4536024b5a7b938e4761ac7e6789840979dcfbe5
usedcoding <ja14759@gmail.com>
2023-10-31 11:48:15 +0900

    수ì정

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import wiseSaying.WiseSayingController;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,164 +9,123 @@ import java.util.Scanner;
 public class App {
     public void run() {
         Scanner sc = new Scanner(System.in);
-        long id = 0;
 
-        Member loginedMember = null;
+        WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
-        List<WiseSaying> wiseSayingList = new ArrayList<>();
-        List<Member> memberList = new ArrayList<>();
 
-        while(true) {
+//        Member loginedMember = null;
+//
+//
+//        List<Member> memberList = new ArrayList<>();
+
+        while (true) {
             System.out.print("명령어 ) ");
 
             String command = sc.nextLine();
 
-            if(command.equals("종료")) {
-                System.out.println("시스템 종료");
-                break;
-
-            }else if (command.equals("회원가입")) {
-                    String userId;
-                    String password;
-                    String confirmPassword;
-
-                while(true) {
-                    System.out.print("아이디 ) ");
-                    userId = sc.nextLine().trim();
-
-                    boolean duplicatedUserId = false;
-
-                    for(int i = 0; i < memberList.size(); i++) {
-                        if(memberList.get(i).getUserId().equals(userId)) {
-                           duplicatedUserId = true;
-                        }
-                    }
-
-                    if(duplicatedUserId) {
-                        System.out.println("이미 존재하는 아이디 입니다.");
-                    }
-                    break;
-                }
-
-                while (true) {
-                    System.out.print("비밀번호 ) ");
-                    password = sc.nextLine().trim();
-
-                    System.out.print("비밀번호 확인 ) ");
-                    confirmPassword = sc.nextLine().trim();
-
-                    for(int i = 0; i < memberList.size(); i++) {
-                        if(!password.equals(confirmPassword)) {
-                            System.out.println("비밀번호가 일치하지 않습니다");
-                        }
-                        continue;
-                    }
-                    break;
-
-                    //비밀번호와 비밀번호 확인에 다른 내용을 작성 하였으나 회원 가입이 완료 되었다고 출력
-
-                }
-                Member member = new Member(userId, password, confirmPassword);
-                memberList.add(member);
-
-
-                System.out.println(userId + " 님 회원가입이 완료 되었습니다.");
-
-
-
-            }else if(command.equals("로그인")){
-
-                boolean checkedUSerId = false;
-                Member member = null;
-                System.out.print("아이디 ) ") ;
-                String userId = sc.nextLine().trim();
-
-                System.out.print("비밀번호 ) ");
-                String password = sc.nextLine().trim();
-
-                for(int i = 0; i < memberList.size(); i++) {
-                    if(memberList.get(i).getUserId().equals(userId)) {
-                        member = memberList.get(i);
-                        checkedUSerId = true;
-                        break;
-                    }
-                }
-
-                if(checkedUSerId = false) {
-                    System.out.println("아이디를 확인해 주세요");
+            switch (command) {
+                case "종료":
+                    System.out.println("시스템 종료");
                     return;
-                }
-
-                if(member.getPassword().equals(password) == false) {
-                    System.out.println("비밀번호가 일치하지 않습니다.");
-                    return;
-                }
-
-                loginedMember = member;
-
-                System.out.println(loginedMember.getUserId() + "님 로그인 되었습니다.");
-
-
-            }else if(command.equals("등록")) {
-                id++;
-
-                System.out.print("제목 ) ");
-                String title = sc.nextLine();
-
-                System.out.print("내용 ) ");
-                String content = sc.nextLine();
-
-                System.out.println(id + "번 게시글이 등록 되었습니다.");
-                WiseSaying wiseSaying = new WiseSaying(id, title, content);
-                wiseSayingList.add(wiseSaying);
-
-            }else if(command.equals("목록")) {
-                System.out.println("번호 / 제목 / 내용");
-
-                for(int i = 0; i < wiseSayingList.size(); i++) {
-                    WiseSaying wiseSaying = wiseSayingList.get(i);
-                    System.out.printf("%d, %s, %s \n", wiseSaying.getId(), wiseSaying.getTitle(), wiseSaying.getContent());
-                }
-
-            }else if(command.equals("삭제")) {
-                System.out.print("삭제할 번호를 입력해 주세요 ) ");
-
-                long removeId = sc.nextLong();
-                sc.nextLine();
-
-                for(int i = 0; i < wiseSayingList.size(); i++) {
-                    WiseSaying wiseSaying = wiseSayingList.get(i);
-
-                    if(removeId == wiseSaying.getId()) {
-                        wiseSayingList.remove(wiseSaying);
-                    }
-                }
-
-                System.out.println(removeId + "번 게시글이 삭제 되었습니다.");
-            }else if(command.equals("수정")) {
-                System.out.print("수정할 번호를 입력해 주세요 ) ");
-                long modifyId = sc.nextLong();
-                sc.nextLine();
-
-                for(int i = 0; i < wiseSayingList.size(); i++) {
-                    WiseSaying wiseSaying = wiseSayingList.get(i);
-
-                    if(modifyId == wiseSaying.getId()) {
-                        System.out.println("기존 제목 : " + wiseSaying.getTitle());
-
-                        System.out.print("수정 : ");
-                        String title = sc.nextLine();
-                        wiseSaying.setTitle(title);
-
-                        System.out.println("기존 내용 : " + wiseSaying.getContent());
-
-                        System.out.print("수정 : ");
-                        String content = sc.nextLine();
-                        wiseSaying.setContent(content);
-                    }
-                }
+                case "등록":
+                    wiseSayingController.create();
+                    break;
+                case "목록":
+                    wiseSayingController.list();
+                    break;
+                case "삭제":
+                    wiseSayingController.remove();
+                    break;
+                case "수정":
+                    wiseSayingController.modify();
+                    break;
             }
 
+//            if (command.equals("회원가입")) {
+//
+//                while (true) {
+//                    System.out.print("아이디 ) ");
+//                   String userId = sc.nextLine().trim();
+//
+//                    boolean duplicatedUserId = false;
+//
+//                    for (int i = 0; i < memberList.size(); i++) {
+//                        if (memberList.get(i).getUserId().equals(userId)) {
+//                            duplicatedUserId = true;
+//                        }
+//                    }
+//
+//                    if (duplicatedUserId) {
+//                        System.out.println("이미 존재하는 아이디 입니다.");
+//                    }
+//                    break;
+//                }
+//
+//                while (true) {
+//                    System.out.print("비밀번호 ) ");
+//                    String password = sc.nextLine().trim();
+//
+//                    System.out.print("비밀번호 확인 ) ");
+//                    String confirmPassword = sc.nextLine().trim();
+//
+//
+//                    if (!password.equals(confirmPassword)) {
+//                        System.out.println("비밀번호가 일치하지 않습니다");
+//                    }
+//                    continue;
+//
+//                }
+//                Member member = new Member(userId, password, confirmPassword);
+//                memberList.add(member);
+//
+//
+//
+//                System.out.println(userId + " 님 회원가입이 완료 되었습니다.");
+//
+//
+//            } else if (command.equals("로그인")) {
+//
+//                boolean checkedUSerId = false;
+//                Member member = null;
+//                System.out.print("아이디 ) ");
+//                String userId = sc.nextLine().trim();
+//
+//                System.out.print("비밀번호 ) ");
+//                String password = sc.nextLine().trim();
+//
+//                for (int i = 0; i < memberList.size(); i++) {
+//                    if (memberList.get(i).getUserId().equals(userId)) {
+//                        member = memberList.get(i);
+//                        checkedUSerId = true;
+//                        break;
+//                    }
+//                }
+//
+//                if (checkedUSerId = false) {
+//                    System.out.println("아이디를 확인해 주세요");
+//                    return;
+//                }
+//
+//                if (member.getPassword().equals(password) == false) {
+//                    System.out.println("비밀번호가 일치하지 않습니다.");
+//                    return;
+//                }
+//
+//                loginedMember = member;
+//
+//                System.out.println(loginedMember.getUserId() + "님 로그인 되었습니다.");
+//
+//
+//            }
+//
+//        }
+
+            sc.close();
         }
     }
 }
+
+
+
+
+
--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -1,5 +1,6 @@
 package org.example;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +8,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+
 public class Member {
     private String userId;
     private String password;
     private String confirmPassword;
+
 
 }
